c75665638c028903ded4100d1959dcf2c754a25a
Ashbel Handiringi <ashbel.handiringi@gmail.com>
2024-05-11 19:03:46 +0200

    Add Sample Json File and read it from asset folder

--- app/src/main/java/com/mopanesystems/myapplication/MainActivity.kt
+++ app/src/main/java/com/mopanesystems/myapplication/MainActivity.kt
@@ -1,15 +1,12 @@
 package com.mopanesystems.myapplication
 
-import android.content.Context
 import android.graphics.Color
 import android.graphics.drawable.GradientDrawable
 import android.os.Bundle
 import android.text.InputType
-import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.AdapterView
-import android.widget.ArrayAdapter
 import android.widget.Button
 import android.widget.FrameLayout
 import android.widget.LinearLayout
@@ -23,14 +20,13 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.core.content.res.ResourcesCompat
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
-import androidx.core.view.marginTop
 import com.google.android.material.textfield.TextInputEditText
 import com.google.android.material.textfield.TextInputLayout
 import com.google.gson.Gson
 import com.mopanesystems.myapplication.Adapters.CustomAdapter
 import com.mopanesystems.myapplication.Models.Fields
 import com.mopanesystems.myapplication.Models.Form
-import com.mopanesystems.myapplication.Models.Options
+import java.io.File
 
 
 class MainActivity : AppCompatActivity() {
@@ -45,315 +41,8 @@ class MainActivity : AppCompatActivity() {
             insets
         }
 
-        val json1 = """
-{
-			"form": "Beekeeping",
-			"steps": [
-				{
-					"title": "Beekeeping",
-					"fields": [
-						{
-							"name": "a5ac708b-f7eb-4a71-b8a7-5afc2039c233",
-							"type": "spinner",
-							"properties": {
-								"text": "Select Ward",
-								"searchable": "Select One"
-							},
-							"required_status": "true:Please specify date",
-							"options": [
-								{
-									"name": "ward_one",
-									"text": "Ward 1"
-								},
-								{
-									"name": "ward_two",
-									"text": "Ward 2"
-								},
-								{
-									"name": "ward_three",
-									"text": "Ward 3"
-								},
-								{
-									"name": "ward_four",
-									"text": "Ward 4"
-								},
-								{
-									"name": "Ward_five",
-									"text": "Ward 5"
-								},
-								{
-									"name": "Ward_Six",
-									"text": "Ward 6"
-								},
-								{
-									"name": "Ward_Seven",
-									"text": "Ward 7"
-								},
-								{
-									"name": "Ward_Eight",
-									"text": "Ward 8"
-								},
-								{
-									"name": "Ward_Nine",
-									"text": "Ward 9"
-								},
-								{
-									"name": "Ward_ten",
-									"text": "Ward 10"
-								},
-								{
-									"name": "Ward_Eleven",
-									"text": "Ward 11"
-								},
-								{
-									"name": "Ward_Twelve",
-									"text": "Ward 12"
-								},
-								{
-									"name": "Ward_Thirteen",
-									"text": "Ward 13"
-								},
-								{
-									"name": "Ward_Sixteen",
-									"text": "Ward 16"
-								},
-								{
-									"name": "Ward_Seventeen",
-									"text": "Ward 17"
-								},
-								{
-									"name": "Ward_Twenty two",
-									"text": "Ward 22"
-								},
-								{
-									"name": "Ward_Twenty three",
-									"text": "Ward 23"
-								}
-							]
-						},
-						{
-							"name": "3d6d9894-baae-4bd6-8e99-51e7f28788d0",
-							"type": "text_input_edit_text",
-							"properties": {
-								"hint": "No. of ToT Refresher Trainings",
-								"type": "name",
-								"input_type": "numberSigned"
-							},
-							"required_status": "true:Please specify value"
-						},
-						{
-							"name": "3c4656f1-8a19-4891-9033-513dc3894376",
-							"type": "text_input_edit_text",
-							"properties": {
-								"hint": "No. Ward level Trainings by Lead Beekeepers",
-								"type": "name",
-								"input_type": "numberSigned"
-							},
-							"required_status": "true:Please specify value"
-						},
-						{
-							"name": "08622b0c-a902-4a17-a67a-65f296867721",
-							"type": "text_input_edit_text",
-							"properties": {
-								"hint": "No. of Male Attendees",
-								"type": "name",
-								"input_type": "numberSigned"
-							},
-							"required_status": "true:Please specify value"
-						},
-						{
-							"name": "745e2545-421b-489e-a628-2fecd2a3b91c",
-							"type": "text_input_edit_text",
-							"properties": {
-								"hint": "No. of Female Attendees",
-								"type": "name",
-								"input_type": "numberSigned"
-							},
-							"required_status": "true:Please specify value"
-						},
-						{
-							"name": "ee734004-6102-4172-979b-2cf19a2a692a",
-							"type": "text_input_edit_text",
-							"properties": {
-								"hint": "Total No. of Training Attendees",
-								"type": "name",
-								"input_type": "numberSigned"
-							},
-							"required_status": "true:Please specify value"
-						},
-						{
-							"name": "aa5e2c42-ef07-4c17-8916-9da53c321136",
-							"type": "radio_group",
-							"properties": {
-								"text": "Attendance Register Yes or No"
-							},
-							"required_status": "true:Please specify date",
-							"options": [
-								{
-									"name": "Yes",
-									"text": "Yes"
-								},
-								{
-									"name": "No",
-									"text": "No"
-								}
-							]
-						},
-						{
-							"name": "dfbd8634-904f-4979-9486-b83dc1efe388",
-							"type": "text_input_edit_text",
-							"properties": {
-								"hint": "Amount Paid (USD${'$'}${'$'}) for Ward base B/Keeping Training (ToT) ",
-								"type": "name",
-								"input_type": "numberDecimal"
-							},
-							"required_status": "true:Please specify value"
-						},
-						{
-							"name": "1cc0dabd-affe-4700-9c08-0deb6baf2580",
-							"type": "text_input_edit_text",
-							"properties": {
-								"hint": "Total No. Beekeepers per Ward",
-								"type": "name",
-								"input_type": "numberDecimal"
-							},
-							"required_status": "true:Please specify value"
-						},
-						{
-							"name": "d9867759-0ca6-4ebc-b976-82d0b023d9de",
-							"type": "text_input_edit_text",
-							"properties": {
-								"hint": "Total No. of Beekeepers who Harvested",
-								"type": "name",
-								"input_type": "numberSigned"
-							},
-							"required_status": "true:Please specify value"
-						},
-						{
-							"name": "7ff03675-d470-44ea-9707-c18dace99126",
-							"type": "text_input_edit_text",
-							"properties": {
-								"hint": "Male Count",
-								"type": "name",
-								"input_type": "numberSigned"
-							},
-							"required_status": "true:Please specify value"
-						},
-						{
-							"name": "ba91fece-7596-433c-b335-6f5b683f75f2",
-							"type": "text_input_edit_text",
-							"properties": {
-								"hint": "Female Count",
-								"type": "name",
-								"input_type": "numberSigned"
-							},
-							"required_status": "true:Please specify value"
-						},
-						{
-							"name": "42d7830c-0797-458d-81bb-5239122bb210",
-							"type": "text_input_edit_text",
-							"properties": {
-								"hint": "Total No. of KTB Occupied",
-								"type": "name",
-								"input_type": "numberDecimal"
-							},
-							"required_status": "true:Please specify value"
-						},
-						{
-							"name": "c69e656a-e4d3-4e1a-b803-ba4f3d94fb03",
-							"type": "text_input_edit_text",
-							"properties": {
-								"hint": "Total No. of Trad Hives Occupied",
-								"type": "name",
-								"input_type": "numberDecimal"
-							},
-							"required_status": "true:Please specify value"
-						},
-						{
-							"name": "462d4b5e-a811-49c9-b921-1709004197ef",
-							"type": "text_input_edit_text",
-							"properties": {
-								"hint": "New KTB Hives Dist.",
-								"type": "name",
-								"input_type": "numberSigned"
-							},
-							"required_status": "true:Please specify value"
-						},
-						{
-							"name": "d37df441-911e-4eae-b425-93af77a7cbd8",
-							"type": "text_input_edit_text",
-							"properties": {
-								"hint": "Beekeeping Kits (Incl. bee suites, smokers etc)",
-								"type": "name",
-								"input_type": "numberSigned"
-							},
-							"required_status": "true:Please specify value"
-						},
-						{
-							"name": "b6e3e51f-930b-46df-9ce4-a70b51dede1b",
-							"type": "text_input_edit_text",
-							"properties": {
-								"hint": "Total Honey Harvested (KTB + Trad) Kgs",
-								"type": "name",
-								"input_type": "numberDecimal"
-							},
-							"required_status": "true:Please specify value"
-						},
-						{
-							"name": "130a8160-0132-422a-9546-12d399e19294",
-							"type": "text_input_edit_text",
-							"properties": {
-								"hint": "Total Amount Sold (Kgs)",
-								"type": "name",
-								"input_type": "numberDecimal"
-							},
-							"required_status": "true:Please specify value"
-						},
-						{
-							"name": "761f3b9a-514b-497a-a2a2-9670398f9e11",
-							"type": "text_input_edit_text",
-							"properties": {
-								"hint": "Avg. Price/kg/USD",
-								"type": "name",
-								"input_type": "numberDecimal"
-							},
-							"required_status": "true:Please specify value"
-						},
-						{
-							"name": "73c6f4d8-812d-4b32-8ccd-54d4b8f24e39",
-							"type": "text_input_edit_text",
-							"properties": {
-								"hint": "Total Income USD${'$'}${'$'}",
-								"type": "name",
-								"input_type": "numberDecimal"
-							},
-							"required_status": "true:Please specify value"
-						},
-						{
-							"name": "84f67453-8c29-4287-a137-9a2e91f453c3",
-							"type": "text_input_edit_text",
-							"properties": {
-								"hint": "Avg. Price/Kg/ZWL",
-								"type": "name",
-								"input_type": "numberDecimal"
-							},
-							"required_status": "true:Please specify value"
-						},
-						{
-							"name": "c206a202-2d45-46c9-8512-b860505b359d",
-							"type": "text_input_edit_text",
-							"properties": {
-								"hint": "Total Income ZWL${'$'}${'$'}",
-								"type": "name",
-								"input_type": "numberDecimal"
-							},
-							"required_status": "true:Please specify value"
-						}
-					]
-				}
-			]
-		}"""
-        val form = Gson().fromJson(json1, Form :: class.java)
+        val jsonString = assets.open("data.json").bufferedReader().use { it.readText() }
+        val form = Gson().fromJson(jsonString, Form :: class.java)
         val fields = form.steps[0].fields
         for (field in fields) {
             when (field.type) {
@@ -380,7 +69,7 @@ class MainActivity : AppCompatActivity() {
         btn.layoutParams = LinearLayout.LayoutParams(
             ViewGroup.LayoutParams.MATCH_PARENT,
             ViewGroup.LayoutParams.WRAP_CONTENT)
-        btn.setText("Click me")
+        btn.text = "Click me"
         btn.setOnClickListener {
             val values = getValues()
             for ((key, value) in values) {
@@ -450,9 +139,9 @@ class MainActivity : AppCompatActivity() {
             AdapterView.OnItemSelectedListener {
             override fun onItemSelected(parent: AdapterView<*>,
                                         view: View, position: Int, id: Long) {
-                Toast.makeText(this@MainActivity,
-                    getString(R.string.selected_item) + " " +
-                            "" + field.options[position], Toast.LENGTH_SHORT).show()
+//                Toast.makeText(this@MainActivity,
+//                    getString(R.string.selected_item) + " " +
+//                            "" + field.options[position], Toast.LENGTH_SHORT).show()
             }
 
             override fun onNothingSelected(parent: AdapterView<*>) {
